a4a3cdffa2bb6d1714d8806e9c1baa754a3c9c2c
unknown <benazetlaurent@gmail.com>
2019-01-18 20:19:12 +0100

    We can now find the first empty case

--- Backtracking/solver.py
+++ Backtracking/solver.py
@@ -1,33 +1,41 @@
-tabToSolve = [[4, 1, 0, 3, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 2, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 5, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 6, 0, 0]]
+# basic grid to solve (correct tests will be made later)
+gridToSolve = [[9, 0, 0, 1, 0, 0, 0, 0, 5],
+               [0, 0, 5, 0, 9, 0, 2, 0, 1],
+               [8, 0, 0, 0, 4, 0, 0, 0, 0],
+               [0, 0, 0, 0, 8, 0, 0, 0, 0],
+               [0, 0, 0, 7, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 2, 6, 0, 0, 9],
+               [2, 0, 0, 3, 0, 0, 0, 0, 6],
+               [0, 0, 0, 2, 0, 0, 9, 0, 0],
+               [0, 0, 1, 9, 0, 4, 5, 7, 0]]
 
-solvingTab = [[0 for i in range(9)] for j in range(9)]
-
-
-def print_solving():
-    print("Tableau en train d'être résolu \n")
-    for a in solvingTab:
-        print(a)
-    print("\n")
-    print(solvingTab)
-    print("\n")
+# They will be used to find the first empty case
+varRow, varCol = 0, 0
 
 
 def print_toSolve():
-    print("Tableau à résoudre \n")
-    for a in tabToSolve:
+    print("Grid to solve \n")
+    for a in gridToSolve:
         print(a)
     print("\n")
-    print(tabToSolve)
-    print("\n")
+
+
+# Allows us to find the coordinates of the first empty case (and assign them to variables defined above)
+def findFreeCase(grid):
+    global varRow
+    global varCol
+    for row in range(9):
+        for col in range(9):
+            if grid[row][col] == 0:
+                varRow = row
+                varCol = col
+                return True
+    return False
 
 
 print_toSolve()
-print_solving()
+print(varCol)
+print(varRow)
+print(findFreeCase(gridToSolve))
+print(varCol)
+print(varRow)
